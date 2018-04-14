c119b5702aab1427f9ade79e7acd62c3bf0c3d82
Phil Weir <phil.weir@flaxandteal.co.uk>
2018-04-14 11:29:31 +0100

    Add a terminfo maker script

---
+++ home/.local/bin/make-tmux-256color-italic.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -f /tmp/tmux-256color-italic.terminfo
+infocmp tmux | sed -e 's/^tmux[^|]*|[^,]*,/tmux-256color-italic|screen with italics support,/' -e 's/%?%p1%t;3%/%?%p1%t;7%/' -e 's/smso=[^,]*,/smso=\E[7m,/' -e 's/rmso=[^,]*,/rmso=\E[27m,/' -e '/$/ sitm=\E[3m, ritm=\E[23m,/' > /tmp/tmux-256color-italic.terminfo
+tic /tmp/tmux-256color-italic.terminfo
